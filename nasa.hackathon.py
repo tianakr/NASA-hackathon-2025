0ff0efaa7a107e2c9343298a8b156b1b9333cde7
tianakr <tanyakrivda@yahoo.com>
2025-10-04 05:20:02 -0700

    added thing in menu

--- nasa.hackathon.py
+++ nasa.hackathon.py
@@ -6,9 +6,10 @@ def menu():
             1. Location
             2. Date
             3. Extreme temperatures
-            4. Rain probability
+            4. Precipitation probability
             5. UV index
-            6. Exit
+            6. Wind
+            7. Exit
             """)
 
 def location():
@@ -17,18 +18,20 @@ def date():
 
 def extreme_temp(date, location):
 
-def rain_probability(date, location):
+def precipitation_probability(date, location):
+    probability_precipitation = (n/t) * 100 #just a formula, will change it later
+    return probability_precipitation
 
 def uv_index():
 
 def exit():
-    return ("Thamk you for using, will it rain on my parade! 🌦️")
+    return ("Thank you for using, will it rain on my parade!")
 
 
 
 choose_option = ""
 
-while choose_option != 6:
+while choose_option != "7":
     print(menu())
     choose_option = input("Choose your option (1-6): ")
 
@@ -39,8 +42,12 @@ elif choose_option == "2":
 elif choose_option == "3":
     extreme_temp(date, location)
 elif choose_option == "4":
-    rain_probability(date, location)
+    precipitation_probability(date, location)
 elif choose_option == "5":
     uv_index()
 elif choose_option == "6":
+    precipitation_probability()   
+elif choose_option == "6":
     print(exit())
+else:
+    print("Invalid choice. Try again!")
