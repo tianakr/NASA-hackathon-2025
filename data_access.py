7aafd2df0529a71e23a7c11afc3995afba733e52
awwabrasulmalik <awwabwajidrasul@gmail.com>
2025-10-05 11:49:44 +0200

    added daily and hourly funcs

--- data_access.py
+++ data_access.py
@@ -6,10 +6,11 @@ from matplotlib import pyplot as plt
 user_lat = float(input("Enter latitude: "))
 user_lon = float(input("Enter longitude: "))
 target_date = input("Enter date (YYYY-MM-DD): ")
-end_date = datetime.strptime((target_date - timedelta(days=1)), "%Y-%m-%d")
+target_time = input("Enter time (HH:MM): ")
+end_date = datetime.strptime(target_date, "%Y-%m-%d") - timedelta(days=1)
 start_date = end_date - timedelta(days=90)
 
-def get_data(lat, lon, start_date, end_date, quantity):
+def get_data_daily(lat, lon, start_date, end_date, quantity):
     start_date = datetime.strftime(start_date, "%Y%m%d")
     end_date = datetime.strftime(end_date, "%Y%m%d")
     list_values = []
@@ -24,8 +25,25 @@ def get_data(lat, lon, start_date, end_date, quantity):
         list_values.append(value)
     return list_values
 
-uv_values = get_data(user_lat, user_lon, start_date, end_date, "ALLSKY_SFC_UV_INDEX")
+def get_data_hourly(lat, lon, start_date, end_date, time, quantity):
+    start_date = datetime.strftime(start_date, "%Y%m%d")
+    end_date = datetime.strftime(end_date, "%Y%m%d")
+    hour = int(time.split(":")[0])
+    list_values = []
 
+    API_URL = f"https://power.larc.nasa.gov/api/temporal/hourly/point?start={start_date}&end={end_date}&latitude=10&longitude=10&community=re&parameters={quantity}&format=json&units=metric&header=true"
+
+    response = req.get(API_URL)
+    data = response.json()
+    dict_data = data["properties"]["parameter"][quantity]
+    
+    for key, value in dict_data.items():
+        if key.endswith(f"{hour:02d}"):
+            list_values.append(value)
+    return list_values
+
+uv_values = get_data_hourly(user_lat, user_lon, start_date, end_date, target_time, "ALLSKY_SFC_UV_INDEX")
+cloud_cover_values = get_data_daily(user_lat, user_lon, start_date, end_date, "CLOUD_OD")
 plt.plot(uv_values)
 
 plt.show()
