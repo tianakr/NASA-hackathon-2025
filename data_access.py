b4a030aaf7e22d998f2a08ea54af3cc7ccb4e962
awwabrasulmalik <awwabwajidrasul@gmail.com>
2025-10-05 11:36:13 +0200

    changed data to not include target date

--- data_access.py
+++ data_access.py
@@ -5,8 +5,8 @@ from matplotlib import pyplot as plt
 
 user_lat = float(input("Enter latitude: "))
 user_lon = float(input("Enter longitude: "))
-user_date = input("Enter date (YYYY-MM-DD): ")
-end_date = datetime.strptime(user_date, "%Y-%m-%d")
+target_date = input("Enter date (YYYY-MM-DD): ")
+end_date = datetime.strptime((target_date - timedelta(days=1)), "%Y-%m-%d")
 start_date = end_date - timedelta(days=90)
 
 def get_data(lat, lon, start_date, end_date, quantity):
@@ -17,14 +17,15 @@ def get_data(lat, lon, start_date, end_date, quantity):
     API_URL = f"https://power.larc.nasa.gov/api/temporal/daily/point?start={start_date}&end={end_date}&latitude=10&longitude=10&community=re&parameters={quantity}&format=json&units=metric&header=true"
 
     response = req.get(API_URL)
-    data_frame = response.json()
-    data = data_frame["properties"]["parameter"][quantity]
+    data = response.json()
+    dict_data = data["properties"]["parameter"][quantity]
     
-    for value in data.values():
+    for value in dict_data.values():
         list_values.append(value)
     return list_values
 
 uv_values = get_data(user_lat, user_lon, start_date, end_date, "ALLSKY_SFC_UV_INDEX")
 
 plt.plot(uv_values)
+
 plt.show()
